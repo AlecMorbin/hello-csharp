193b705c992eb41fef3529324b23bfd78ad95a27
Alec <alemor98@hotmail.it>
2022-03-31 13:05:28 +0200

    Termine esercizio

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Benvenuto!");
+
+Console.WriteLine("gennaio");
+Console.WriteLine("febbraio");
+Console.WriteLine("marzo");
+Console.WriteLine("aprile");
+Console.WriteLine("maggio");
+Console.WriteLine("giugno");
+Console.WriteLine("luglio");
+Console.WriteLine("agosto");
+Console.WriteLine("settembre");
+Console.WriteLine("ottobre");
+Console.WriteLine("novembre");
+Console.WriteLine("dicembre");
+
+Console.WriteLine("Quale è il tuo preferito?");
+
+string input = Console.ReadLine();
+
+Console.WriteLine("Hai scritto: "+input);
+
+Console.WriteLine("Arrivederci!");
